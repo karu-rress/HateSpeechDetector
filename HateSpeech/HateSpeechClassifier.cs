7a14e779d333feae2fdeb88f7ae2f703225d1b19
Karu <nsun527@naver.com>
2022-04-10 21:56:03 +0900

    second

---
+++ HateSpeech/HateSpeechClassifier.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HateSpeech;
+
+public enum Bias
+{
+    [Description("Gender Bias")]
+    Gender,
+    [Description("Other Bias")]
+    Others,
+    [Description("None")]
+    None,
+}
+
+public enum Hate
+{
+    [Description("Hate Speech")]
+    Hate,
+    [Description("Offensive")]
+    Offensive,
+    [Description("None")]
+    None
+}
+
+public class HateSpeechClassifier
+{
+    public string? Comment { get; set; }
+
+    private GenderBias.ModelInput genderInput;
+    private GenderBias.ModelOutput genderOutput;
+
+    private BiasModel.ModelInput biasInput;
+    private BiasModel.ModelOutput biasOutput;
+
+    private HateModel.ModelInput hateInput;
+    private HateModel.ModelOutput hateOutput;
+
+    public bool HasGenderBias { get; private set; }
+    public Bias BiasType { get; private set; }
+    public bool HasBias => BiasType is not Bias.None;
+    public Hate HateType { get; private set; }
+    public bool HasHate => HateType is not Hate.None;
+    public bool IsClean => !HasBias && !HasHate;
+
+    public void Predict(string comment)
+    {
+        if (string.IsNullOrEmpty(comment))
+            return;
+
+        Comment = comment;
+
+        genderInput = new() { Comments = Comment };
+        biasInput = new() { Comments = Comment };
+        hateInput = new() { Comments = Comment };
+
+        genderOutput = GenderBias.Predict(genderInput);
+        biasOutput = BiasModel.Predict(biasInput);
+        hateOutput = HateModel.Predict(hateInput);
+
+        HasGenderBias = genderOutput.Prediction;
+        BiasType = biasOutput.Prediction switch
+        {
+            "gender" => Bias.Gender,
+            "others" => Bias.Others,
+            "none" => Bias.None,
+            _ => throw new Exception()
+        };
+        HateType = hateOutput.Prediction switch
+        {
+            "hate" => Hate.Hate,
+            "offensive" => Hate.Offensive,
+            "none" => Hate.None,
+            _ => throw new Exception()
+        };
+    }
+}
--- HateSpeech/Program.cs
+++ HateSpeech/Program.cs
@@ -10,42 +10,30 @@ static extern bool SetConsoleCP(uint wCodePageID);
 SetConsoleCP(949);
 SetConsoleOutputCP(949);
 
+
+/* bias / hate
+ * others / hate
+ * none / none
+ * gender / hate
+ * none / offensive
+ * 
+ */
+
 void Predict(string comment)
 {
-    BiasModel.ModelInput biasInput = new() { Comments = comment };
-    BiasModel.ModelOutput biasOutput = BiasModel.Predict(biasInput);
-    string bias = biasOutput.Prediction;
+    HateSpeechClassifier classifier = new();
+    classifier.Predict(comment);
 
-    GenderBias.ModelInput genderInput = new() { Comments = comment };
-    GenderBias.ModelOutput genderOutput = GenderBias.Predict(genderInput);
-    bool gender = genderOutput.Prediction;
-
-    HateModel.ModelInput hateInput = new() { Comments = comment };
-    HateModel.ModelOutput hateOutput = HateModel.Predict(hateInput);
-    string hate = hateOutput.Prediction;
-
-    var hate_speech = false;
-
-    if (gender)
-    {
+    if (classifier.HasGenderBias)
         Console.WriteLine("성차별 표현이 포함되어 있습니다.");
-        hate_speech = true;
-    }
 
+    if (classifier.HasBias)
+        Console.WriteLine($"{classifier.BiasType} 타입의 차별 표현이 있습니다.");
 
-    if (bias is not "none")
-    {
-        Console.WriteLine($"{bias} 타입의 차별 표현이 있습니다.");
-        hate_speech = true;
-    }
+    if (classifier.HasHate)
+        Console.WriteLine($"{classifier.HateType} 타입의 혐오 표현이 있습니다.");
 
-    if (hate is not "none")
-    {
-        Console.WriteLine($"{hate} 타입의 혐오 표현이 있습니다.");
-        hate_speech = true;
-    }
-
-    if (hate_speech is false)
+    if (classifier.IsClean)
         Console.WriteLine("혐오 표현이 감지되지 않았습니다.");
 }
 
